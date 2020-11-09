a283119a5ee5643aedaafd136a7b41ab079f4fe9
Clovis Gladstone <clovisgladstone@gmail.com>
2020-11-09 10:03:13 -0600

    add intro to hub on landing page

--- web-app/src/App.vue
+++ web-app/src/App.vue
@@ -4,15 +4,9 @@
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>
-                    <b-nav-item href="https://intertextual-hub.org"
-                        >Home</b-nav-item
-                    >
-                    <b-nav-item href="https://artfl-project.uchicago.edu"
-                        >ARTFL Project</b-nav-item
-                    >
-                    <b-nav-item href="https://textual-optics-lab.uchicago.edu"
-                        >Textual Optics Lab</b-nav-item
-                    >
+                    <b-nav-item href="https://intertextual-hub.org">Home</b-nav-item>
+                    <b-nav-item href="https://artfl-project.uchicago.edu">ARTFL Project</b-nav-item>
+                    <b-nav-item href="https://textual-optics-lab.uchicago.edu">Textual Optics Lab</b-nav-item>
                     <b-nav-item href="https://neh.gov">NEH</b-nav-item>
                 </b-navbar-nav>
             </b-collapse>
@@ -32,33 +26,46 @@
             >
         </b-navbar>
         <b-tabs fill no-fade v-model="tabIndex" id="main-tabs" class="shadow">
-            <b-tab
-                class="position-absolute shadow module-tab"
-                title="Collections"
-                @click="selectModule('home')"
-            >
+            <b-tab class="position-absolute shadow module-tab" title="Collections" @click="selectModule('home')">
                 <transition name="slide-fade">
-                    <b-card style="border-top-width: 0" v-if="show">
-                        <h5>Collections included in the Intertextual Hub:</h5>
-                        <b-list-group style="width: fit-content">
-                            <b-list-group-item
-                                v-for="philoDb in philoDbs"
-                                :key="philoDb.name"
-                            >
-                                <a :href="philoDb.url" target="_blank">{{
-                                    philoDb.name
-                                }}</a>
-                                :
-                                <span v-html="philoDb.description"></span>
-                            </b-list-group-item>
-                        </b-list-group>
+                    <b-card no-body class="px-4 pb-4" style="border-top-width: 0" v-if="show">
+                        <b-row>
+                            <b-col cols="12" md="6" lg="7">
+                                <h5 class="mt-4">Explore specific collection under PhiloLogic:</h5>
+                                <b-list-group style="width: fit-content">
+                                    <b-list-group-item v-for="philoDb in philoDbs" :key="philoDb.name">
+                                        <a :href="philoDb.url" target="_blank">{{ philoDb.name }}</a>
+                                        :
+                                        <span v-html="philoDb.description"></span>
+                                    </b-list-group-item>
+                                </b-list-group> </b-col
+                            ><b-col cols="12" md="6" lg="5">
+                                <h5 class="mt-4">What is the Intertextual Hub?</h5>
+                                <p class="text-justify">
+                                    The Intertextual Hub is an experimental digital humanities reading environment that
+                                    aims to situate specific documents in their broader context of intertextual
+                                    relations, whether in the form of direct or indirect borrowings, shared topics with
+                                    other texts or parts of texts, or other kinds of lexical similarity. Intuitively, we
+                                    believe that the conceptual relationships discovered by text mining algorithms among
+                                    texts in large, heterogeneous collections can fruitfully inform and guide
+                                    traditional close-reading approaches. More fundamentally, our contention is that
+                                    scholarly reading in the digital age—and the true usefulness of computational
+                                    analysis of texts—should be foregrounded on the discovery and navigation of
+                                    intertextual relationships. The model we have developed here allows users to
+                                    navigate between individual and larger groups of texts that are related through
+                                    shared themes, ideas, and passages. What the Intertextual Hub offers, then, along
+                                    with the scalable reading tools, is an approach to federating collections that can
+                                    bypass the various competing problems of quality (OCR vs. curated) and access (pay
+                                    vs. public) inherent in digital collections today, and still yield meaningful
+                                    results.
+                                </p>
+                            </b-col></b-row
+                        >
                     </b-card>
                 </transition>
                 <div class="footer" v-if="routeName != 'TextNavigation'">
                     <b-container>
-                        <p class="mb-0" style="font-variant: small-caps">
-                            Sponsored by the
-                        </p>
+                        <p class="mb-0" style="font-variant: small-caps">Sponsored by the</p>
                         <a href="https://neh.gov" target="_blank">
                             <img
                                 :src="`${publicPath}NEH-Preferred-Seal820.jpg`"
@@ -107,12 +114,7 @@
             </b-tab>
         </b-tabs>
         <transition name="fade">
-            <div
-                id="show"
-                class="p-2 position-absolute"
-                v-if="!show"
-                @click="showOptions()"
-            >
+            <div id="show" class="p-2 position-absolute" v-if="!show" @click="showOptions()">
                 Click a tab to show navigation options
             </div>
         </transition>
